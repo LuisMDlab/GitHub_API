6794a9fb716ce2db03b814ee5f8b6d5a07bf4a8e
LuisMDlab <luisfelipeprf@gmail.com>
2017-07-20 09:47:49 -0300

    Update GITissue_create.py

--- GITissue_create.py
+++ GITissue_create.py
@@ -1,25 +1,27 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jul 14 12:30:28 2017
-
 @author: MlabL3P
 """
+#Script to Insert Issues into GitHub.
 
-from github import Github
-from config import token_dict
+from github import Github #GitHub Module to connect with API.
+from config import token_dict #External file with a dict of colaborators tokens.
 import csv
+import time
 
-with open('SocialTeste.csv', encoding = "utf-8") as csvfile:
+#Import issue info from csv. (Exported of MANTIS)
+with open('Issue_Teste.csv', encoding = "utf-8") as csvfile:
     reader = csv.DictReader(csvfile)
-    
     for row in reader:
-              
+        #Comparation info dicts.
         situacao = {'corrigido':'closed', 'reaberto':'open', 'aberto':'open'}
         
-        label = {'imediato': '?', 'urgente': '?', 'alta': '?', 'normal': '?', 'baixa':'?', #Prioridade
-                 'travamento':'?', 'grande': '?', 'pequeno':'?', 'mínimo':'?',  #Gravidade
-                 'ajuste':'?','correção de erro':'?', 'Design':'?', 'Documentação':'', 'Estudo':'?', 'Geral':'?', 'Infra-estrutura':'?', 'Melhoria':'?' ,'Nova Funcionalidade': '?', 'Teste':'?'} #Categoria
+        label = {'urgente':'piority:critical', 'alta':'piority:high', 'normal':'piority:medium', 'baixa':'piority:low', #Prioridade
+                 'Ajuste':'Bug','Correção de Erro':'Bug', 'Design':'Interface', 'Documentação':'Docs', 'Estudo':'Estudo', 'Infra-estrutura':None, 'Melhoria':'Improvement' ,'Nova Funcionalidade':'NewFeature', 'Teste':'None', #Categoria
+                 'atribuido':'dev:ready', 'novo':'', 'resolvido':'dev:validation', 'retorno':'dev:inprogress'} #Estado
         
+        #Colaborators Dict.
         colaboradores = {'leogermani':Github(token_dict['token_leo']),
                          'luis':Github(token_dict['token_luis']),
                          'weryqyes':Github(token_dict['token_wery']),
@@ -30,13 +32,18 @@ with open('SocialTeste.csv', encoding = "utf-8") as csvfile:
                          'rodrigo':Github(token_dict['token_rodrigo']),
                          'mbrunodm':Github(''),
                          'andre':Github(token_dict['token_andre']),
-                         '':'None'}
-        
+                         '':None}
+        #Connection with repo. (Access Tokens of repository users owners is needed)
         repo = colaboradores[row['Relator']].get_organization('medialab-ufg').get_repo('test-issues')
         
-       
-        issue = repo.create_issue(title = row['Resumo'], body = 'Feito por ' + row['Relator'],\
-                                  assignee = colaboradores[row['Atribuido']].get_user().login,\
-                                  label = (label[row['Categoria']], label[row['Prioridade']], label[row['Gravidade']]))
-        
-        issue.edit(state=situacao[row['Resolução']])
+        try:
+            issue = repo.create_issue(title = row['Resumo'],\
+                                      body = row['Resumo'],\
+                                      assignee = colaboradores[row['Atribuido']].get_user().login,\
+                                      labels = [label[row['Prioridade']], label[row['Categoria']], label[row['Estado']]])
+        except:
+            issue = repo.create_issue(title = row['Resumo'],\
+                                      body = row['Resumo'],\
+                                      labels = [label[row['Prioridade']], label[row['Categoria']], label[row['Estado']]], created_at = '2007-01-09')
+        #Avoid API Stop.
+        time.sleep(5)
