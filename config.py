32f8cdfdbbe6cb7aee653d9aeb1528986e780dd1
LuisMDlab <luisfelipeprf@gmail.com>
2017-07-21 11:03:31 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+token_dict = {
+        'token_leo': 'Insert Token Here',
+        'token_luis': 'Insert Token Here',
+        'token_wery': 'Insert Token Here',
+        'token_rodrigo': 'Insert Token Here',
+        'token_eduardo':'Insert Token Here',
+        'token_julianny': 'Insert Token Here',
+        'token_wallison': 'Insert Token Here',
+        'token_andre': 'Insert Token Here',
+        'token_mbruno':'Insert Token Here'}
